9d244b8e79269c2ef21fe2151089b1f2f6012937
mats-js <100519629+mats-js@users.noreply.github.com>
2022-11-02 11:54:33 +0100

    Added firebase/firestore support

--- components/Chat.js
+++ components/Chat.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+const firebase = require('firebase');
+require('firebase/firestore');
 
 export default class Chat extends React.Component {
   constructor() {
     super();
     this.state = {
       messages: [],
+      uid: 0,
+      user: {
+        _id: '',
+        avatar: '',
+        name: '',
+      },
     };
+
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        apiKey: 'AIzaSyD79_elAJMOK5ieEmlMAkD0XPgLrfqh3uE',
+        authDomain: 'chat-app-500f0.firebaseapp.com',
+        projectId: 'chat-app-500f0',
+        storageBucket: 'chat-app-500f0.appspot.com',
+        messagingSenderId: '467477866860',
+        appId: '1:467477866860:web:6c8f513fbd0f9d7bfb2b49',
+      });
+    }
+
+    this.referenceChatMessages = firebase.firestore().collection('messages');
   }
 
   componentDidMount() {
@@ -16,16 +37,6 @@ export default class Chat extends React.Component {
 
     this.setState({
       messages: [
-        {
-          _id: 1,
-          text: `Hi ${name}, how are you today?`,
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
         {
           _id: 2,
           text: `${name} has entered the chat`,
@@ -36,13 +47,56 @@ export default class Chat extends React.Component {
     });
 
     this.props.navigation.setOptions({ title: name });
+
+    this.referenceChatMessages = firebase.firestore().collection('messages');
+    this.unsubscribe = this.referenceChatMessages.onSnapshot(
+      this.onCollectionUpdate
+    );
+
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+      this.setState({
+        uid: user.uid,
+        messages: [],
+        user: {
+          _id: user.uid,
+          name: name,
+        },
+      });
+      this.unsubscribe = this.referenceChatMessages
+        .orderBy('createdAt', 'desc')
+        .onSnapshot(this.onCollectionUpdate);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+    this.authUnsubscribe();
   }
 
   onSend(messages = []) {
-    this.setState((previousState) => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }));
+    this.setState(
+      (previousState) => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }),
+      () => {
+        this.addMessage();
+      }
+    );
   }
+
+  addMessage = () => {
+    const message = this.state.messages[0];
+    this.referenceChatMessages.add({
+      uid: this.state.uid,
+      _id: message._id,
+      text: message.text || '',
+      createdAt: message.createdAt,
+      user: message.user,
+    });
+  };
 
   renderBubble(props) {
     return (
@@ -60,6 +114,28 @@ export default class Chat extends React.Component {
     );
   }
 
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+          avatar: data.user.avatar || '',
+        },
+      });
+    });
+    this.setState({
+      messages,
+    });
+  };
+
   render() {
     // Set the color property as background color for the chat screen
     let color = this.props.route.params.color;
@@ -70,7 +146,8 @@ export default class Chat extends React.Component {
           messages={this.state.messages}
           onSend={(messages) => this.onSend(messages)}
           user={{
-            _id: 1,
+            _id: this.state.uid,
+            avatar: 'https://placeimg.com/140/140/any',
           }}
         />
         {Platform.OS === 'android' ? (
